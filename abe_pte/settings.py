41289b74b111983d5e086587e9a36d062041cb87
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-10-23 14:11:45 +0530

    updated csrf

--- abe_pte/settings.py
+++ abe_pte/settings.py
@@ -138,7 +138,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://admin.abepte.accoladesweb.com",
+]

f0e85f2cbe29d3ff899440ddf0a7fe4e8857aaed
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-10-24 16:41:34 +0530

    test api

--- mocktest/views.py
+++ mocktest/views.py
@@ -2,22 +2,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import MockTest, MockTestSection, SubSection
+from .models import MockTest, MockTestSection, SubSection,Section
 from .serializers import MockTestSectionSerializer
 
 class MockTestAllSectionsAPIView(APIView):
-    def get(self, request, mocktest_id):
+    def get(self, request):
         try:
-            mock_test = MockTest.objects.get(id=mocktest_id)
-        except MockTest.DoesNotExist:
+            section = Section.objects.all()
+        except Section.DoesNotExist:
             return Response({"error": "Mock test not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        # Get all linked sections
-        mock_sections = MockTestSection.objects.filter(mock_test=mock_test).select_related('section').order_by('order')
-        serializer = MockTestSectionSerializer(mock_sections, many=True)
-
         return Response({
-            "mock_test": mock_test.title,
-            "sections_count": mock_sections.count(),
-            "sections": serializer.data
+            
+            "sections_count": section.count(),
+            "sections": section
         })

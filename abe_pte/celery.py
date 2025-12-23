0e84f67c3ba91cb24ad0cdfc14d45266a01c742d
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-12-23 10:10:47 +0530

    updated celery

--- abe_pte/celery.py
+++ abe_pte/celery.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from dotenv import load_dotenv
 
+load_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','abe_pte.settings')
 app = Celery('abe_pte')
 app.config_from_object('django.conf:settings', namespace='CELERY')

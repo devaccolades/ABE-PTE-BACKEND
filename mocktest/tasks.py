38983a422f63154ea1323a72568c5593593a5b20
aswinaccolades <aswink.accolades@gmail.com>
2026-01-02 19:11:12 +0530

    run evaluation

--- mocktest/tasks.py
+++ mocktest/tasks.py
@@ -105,6 +105,7 @@ def evaluate_user_response(user_answer_id, question_id):
         if subsection_obj.ai_input_type == "audio":
             evaluation_payload["transcribed_audio_data"] = user_answer.transcribed_audio_data
 
+        print("Running evaluation orchestrator...", evaluation_payload)
         # if not user_answer.answer_data:
         #     return {"error": "No answer_data provided to evaluate"}
 

195b1447ae9138e639ff1bb4c2495839de760277
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2026-01-21 15:35:38 +0530

    updated question api

--- mocktest/urls.py
+++ mocktest/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    
     path('start-test/', StartMockTestAPIView.as_view(), name='start-test'),
     path('get-question/', GetQuestionAPIView.as_view(), name='get-question'),
-    path('testing/', APIListingQuestions.as_view(), name='test'),
+    path('question/', APIListingQuestions.as_view(), name='question'),
     path('user-response/', UserResponseAPIView.as_view(), name='user-response'),
     path('mocktest-list/', MockTestListAPIView.as_view(), name='mock-test'),
 
